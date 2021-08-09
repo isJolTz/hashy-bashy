98e1c3c2ef33999dd23ec350a15072625785f0b1
Kevin\isJolTz <kdillo893@gmail.com>
2021-08-09 16:22:35 -0500

    Starting With Spring, super basics

---
+++ src/main/java/com/personal/hashy/HashyApplication.java
@@ -0,0 +1,13 @@
+package com.personal.hashy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/hashy/HashyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.hashy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
